3222b34db6928fcf7ebd17470682b1b89aa11318
stephane <paulkelele@gmail.com>
2022-12-28 17:51:08 +0100

    add windoww

--- src/root/Main.java
+++ src/root/Main.java
@@ -1,7 +1,33 @@
 package root;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.IOException;
+
+public class Main extends JFrame {
+    private static Main m = new Main();
+
+    public static void main(String[] args) throws IOException {
+        m.launch();
+       }
+
+    private void launch() throws IOException {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+            UIManager.put("OptionPane.noButtonText", "Non");
+            UIManager.put("OptionPane.yesButtonText", "Oui");
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize UIManager");
+        }
+        m.setIconImage(ImageIO.read(getClass().getResource("/resources/images/france.png")));
+        m.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        m.setLocationRelativeTo(null);
+        m.setSize(900, 700);
+        m.setExtendedState(MAXIMIZED_BOTH);
+        m.setVisible(true);
     }
+
+
 }
